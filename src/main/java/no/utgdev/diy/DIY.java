ce456ead8d6cee5caa7dd0edbe92f0b22a663e39
Nicklas Utgaard <nutgaard@gmail.com>
2019-08-25 17:48:51 +0200

    LB Del 2

--- src/main/java/no/utgdev/diy/DIY.java
+++ src/main/java/no/utgdev/diy/DIY.java
@@ -1,8 +1,17 @@
 package no.utgdev.diy;
 
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 // TODO Oppgave 2
 @SuppressWarnings("ALL")
 public class DIY {
+    private static DIY instance;
+    private boolean instansiated = false;
+    private Set<Method> beans = new HashSet<>();
+    private Map<String, Object> objects;
 
     public static DIY getInstance() {
         return getInstance(false);
@@ -13,18 +22,28 @@ public class DIY {
     }
 
     public static DIY getInstance(boolean refresh) {
-        return null;
+        if (instance == null || refresh) {
+            instance = new DIY();
+        }
+        return instance;
     }
 
 
     public static void load(String classPath) {
-
+        getInstance().beans.addAll(DIYStatic.scan(classPath));
     }
 
     public static void load(Class<?> root) {
+        getInstance().beans.addAll(DIYStatic.scan(root));
     }
 
     public static Object getBean(String name) {
-        return null;
+        DIY diy = getInstance();
+        if (!diy.instansiated) {
+            diy.objects = DIYStatic.instansiate(diy.beans);
+            DIYStatic.wireup(diy.objects);
+            diy.instansiated = true;
+        }
+        return DIYStatic.getBean(diy.objects, name);
     }
 }
